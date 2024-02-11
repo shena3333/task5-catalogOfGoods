7191570d46079eafe03f7a80df3251276b2d2aee
shena3333 <shena3333@mail.ru>
2024-02-11 13:37:34 +0300

    add files

---
+++ script.js
@@ -0,0 +1,63 @@
+//создаем массив с товарами
+let goods = [
+    { id: 1, name: 'шапка', category: 'одежда', price: 500 },
+    { id: 2, name: 'шарф', category: 'одежда', price: 400 },
+    { id: 3, name: 'перчатки', category: 'одежда', price: 300 },
+    { id: 4, name: 'барби', category: 'игрушки', price: 600 },
+    { id: 5, name: 'мяч', category: 'игрушки', price: 200 },
+    { id: 6, name: 'робот', category: 'игрушки', price: 700 },
+    { id: 7, name: 'медведь', category: 'игрушки', price: 100 },
+];
+
+//отображаем форму добавления товара
+function addFormForNewGoods() {
+    let formAddGoods = document.querySelector('.form-add-goods');
+    formAddGoods.className = 'form-add-goods-new';
+    // console.log(formAddGoods);
+}
+// создаю новый товар путем добавление объекта в массив
+function addGoods() {
+    event.preventDefault();// обнули стандартные свойства формы, чтоб не происходило переотрисовка дом-дерево
+    let newGoods = {};
+    let newId;// cоздала переменную для новго идент
+    let arrayId = [];// создала переменную под массив с имеющимися уже идент
+    for (let i = 0; i < goods.length; i++) {
+        arrayId.push(goods[i].id)
+    };
+    newId = Math.max(...arrayId) + 1;// так как функция макс не работает напрямую с массивами , поэтому ... ставлю
+    // console.log('идент для нового товара', newId);
+    newGoods.id = newId;
+    // let productName=document.getElementById('productName');
+    let productName = document.getElementById('productName');
+    newGoods.name = productName.value;
+    let productCategory = document.getElementById('productCategory');
+    newGoods.category = productCategory.value;
+
+    let productPrice = document.getElementById('productPrice');
+    // console.log(productPrice);
+    newGoods.price = productPrice.value;
+
+    // console.log(newGoods);
+    goods.push(newGoods);
+    console.log(goods);
+}
+
+
+// закрытие формы добавления товара
+function closeFormAdd() {
+    event.preventDefault();// обнули стандартные свойства формы, чтоб не происходило переотрисовка дом-дерево
+    let formAddGoods = document.querySelector('.form-add-goods-new');
+    formAddGoods.className = 'form-add-goods';
+}
+
+{/* <div class="info-product">
+<div class="id-product element-info"></div>
+<div class="name-product element-info"></div>
+<div class="prise-product element-info"></div>
+<div class="category-product element-info"></div>
+</div> */}
+
+//достаем div, в который будем класть товары
+let catalog = document.querySelector('.catalog');
+// console.log(catalog);
+
