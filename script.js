976197b833ebf135f765539a40a5623607a0d848
shena3333 <shena3333@mail.ru>
2024-02-13 13:25:52 +0300

    redact

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//создаем массив с товарами
+//массив с товарами
 let goods = [
     // { id: 1, name: 'шапка', category: 'одежда', price: 500 },
     // { id: 2, name: 'шарф', category: 'одежда', price: 400 },
@@ -14,58 +14,78 @@ function addFormForNewGoods() {
     let formAddGoods = document.querySelector('.form-add-goods');
     formAddGoods.className = 'form-add-goods-new';
     // console.log(formAddGoods);
-}
-// создаю новый товар путем добавление объекта в массив
+};
+// достаем catalog, в котором буду отрисовывать элементы
+let catalog = document.querySelector('.catalog');
+
+// создаю новый товар 
 function addGoods() {
     event.preventDefault();// обнули стандартные свойства формы, чтоб не происходило переотрисовка дом-дерево
     let newGoods = {};
     let newId;// cоздала переменную для новго идент
     let arrayId = [];// создала переменную под массив с имеющимися уже идент
+    // собираем все имеющиеся идент
     for (let i = 0; i < goods.length; i++) {
         arrayId.push(goods[i].id)
     };
+    // ставлю идент 1 или следующий за максимальным
     if (arrayId.length == 0) {
         newId = 1
     } else {
         newId = Math.max(...arrayId) + 1;// так как функция макс не работает напрямую с массивами , поэтому ... ставлю
     }
+    //собираю все свойства объекта товара
     newGoods.id = newId;
     let productName = document.getElementById('productName');
     newGoods.name = productName.value;
     let productCategory = document.getElementById('productCategory');
     newGoods.category = productCategory.value;
-
     let productPrice = document.getElementById('productPrice');
-    // console.log(productPrice);
     newGoods.price = productPrice.value;
-
-    // console.log(newGoods);
+    // console.log (newGoods)
+    //все товары собираем в массив
     goods.push(newGoods);
-    console.log(goods);
+    //создаю и заполняю div для нового товара
     let newProduct = document.createElement('div');
     newProduct.className = 'new-prod'
     newProduct.textContent = `${productName.value} - ${productCategory.value} - ${productPrice.value}руб.`;
-   
-   // содание кнопки удаления текущего товара
+    // содание кнопки удаления текущего товара
     let buttonDelete = document.createElement('button');
-    buttonDelete.className='delete-product';
-    buttonDelete.textContent='удалить';
+    buttonDelete.className = 'delete-product';
+    buttonDelete.textContent = 'удалить';
+    // присваиваю кнопке удалить id равный идентификатору в объекте товар
+    buttonDelete.id = newId;
     newProduct.append(buttonDelete);
     // готовый товар с его кнопкой отоброжаем
     catalog.append(newProduct);
-    
+    // навешиваю функционал на кнопку удаления
+    let butDelete = document.querySelectorAll('.delete-product');
+    // console.log(butDelete);
 
 
-    
+
+    // Н Е    Р А Б О Т А Е Т. удаляет все элементы от номера и больше
+    for (let i = 0; i < butDelete.length; i++) {
+        butDelete[i].addEventListener('click', deleteProduct);
+        function deleteProduct() {
+            let currentGoods = document.querySelectorAll('.new-prod');
+            // currentGoods[i] удалить
+            if (butDelete[i].id == goods[i].id) {
+                catalog.removeChild(currentGoods[i])
+            }
+        };
+    }
 
 }
+
+
 // закрытие формы добавления товара
 function closeFormAdd() {
     event.preventDefault();// обнули стандартные свойства формы, чтоб не происходило переотрисовка дом-дерево
     let formAddGoods = document.querySelector('.form-add-goods-new');
     formAddGoods.className = 'form-add-goods';
 }
-// достаем catalog, в котором буду отрисовывать элементы
-let catalog = document.querySelector('.catalog');
-console.log(catalog);
+// фильтр отображения добавленных товаров
+let filterGoods = document.querySelector('.filter-goods');
+console.log(filterGoods.value)
 
